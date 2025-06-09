9868647599396066e7dc7a6ab623b64ea8aeb7ff
Tom Tanticle <113810920+Tanticle@users.noreply.github.com>
2025-06-09 22:06:54 +0200

    Kinda fixed translations.

--- src/main/java/tld/unknown/mystery/items/blocks/CrystalBlockItem.java
+++ src/main/java/tld/unknown/mystery/items/blocks/CrystalBlockItem.java
@@ -1,12 +1,16 @@
 package tld.unknown.mystery.items.blocks;
 
 import lombok.Getter;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import tld.unknown.mystery.api.ThaumcraftData;
+import tld.unknown.mystery.api.aspects.Aspect;
 import tld.unknown.mystery.api.aspects.AspectContainerItem;
 import tld.unknown.mystery.blocks.CrystalBlock;
 import tld.unknown.mystery.data.aspects.AspectList;
 import tld.unknown.mystery.registries.ConfigBlocks;
+import tld.unknown.mystery.util.RegistryUtils;
 
 public class CrystalBlockItem extends BlockItem implements AspectContainerItem {
 
@@ -21,5 +25,12 @@ public class CrystalBlockItem extends BlockItem implements AspectContainerItem {
     @Override
     public AspectList getAspects(ItemStack stack) {
         return new AspectList().add(aspect.getId(), 15);
+    }
+
+    @Override
+    public Component getName(ItemStack pStack) {
+        Component aspect = Aspect.getName(RegistryUtils.access(), getAspect().getId(), false, true);
+        Component text = Component.translatable("block.%s", ThaumcraftData.Blocks.CRYSTAL_COLONY.toLanguageKey());
+        return Component.empty().append(aspect).append(" ").append(text);
     }
 }
