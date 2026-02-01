943c5538dbf9c76097ce917799c131a15d1fc6c8
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2026-01-31 17:39:27 -0700

    w

--- src/main/java/art/arcane/thaumcraft/items/tools/ElementalAxeItem.java
+++ src/main/java/art/arcane/thaumcraft/items/tools/ElementalAxeItem.java
@@ -1,5 +1,10 @@
 package art.arcane.thaumcraft.items.tools;
 
+import art.arcane.thaumcraft.api.ThaumcraftMaterials;
+import art.arcane.thaumcraft.api.enums.InfusionEnchantments;
+import art.arcane.thaumcraft.items.components.InfusionEnchantmentComponent;
+import art.arcane.thaumcraft.registries.ConfigItemComponents;
+import art.arcane.thaumcraft.util.EntityUtils;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
@@ -14,71 +19,67 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
-import art.arcane.thaumcraft.api.enums.InfusionEnchantments;
-import art.arcane.thaumcraft.api.ThaumcraftMaterials;
-import art.arcane.thaumcraft.items.components.InfusionEnchantmentComponent;
-import art.arcane.thaumcraft.registries.ConfigItemComponents;
-import art.arcane.thaumcraft.util.EntityUtils;
 
 import java.util.List;
 import java.util.Map;
 
+// TODO: AXE Bubbles look like ass lmao, all jumpy. should be a trail. and magnet should pull UP as well, to properly get blocks stuck (also axe items should float through other blocks.
 public class ElementalAxeItem extends AxeItem {
 
-    private static final int MAX_USE_DURATION = 72000;
+	private static final int MAX_USE_DURATION = 72000;
 
-    public ElementalAxeItem(Properties props) {
-        super(ThaumcraftMaterials.Tools.ELEMENTAL, ThaumcraftMaterials.Tools.ELEMENTAL.attackDamageBonus(), ThaumcraftMaterials.Tools.ELEMENTAL.speed(),
-                props.rarity(Rarity.RARE).component(
-                        ConfigItemComponents.INFUSION_ENCHANTMENT.value(), new InfusionEnchantmentComponent(Map.of(
-                                InfusionEnchantments.COLLECTOR, (byte)1,
-                                InfusionEnchantments.BURROWING, (byte)1))));
-    }
+	public ElementalAxeItem(Properties props) {
+		super(ThaumcraftMaterials.Tools.ELEMENTAL, ThaumcraftMaterials.Tools.ELEMENTAL.attackDamageBonus(), ThaumcraftMaterials.Tools.ELEMENTAL.speed(),
+				props.rarity(Rarity.RARE).component(
+						ConfigItemComponents.INFUSION_ENCHANTMENT.value(), new InfusionEnchantmentComponent(Map.of(
+								InfusionEnchantments.COLLECTOR, (byte) 1,
+								InfusionEnchantments.BURROWING, (byte) 1))));
+	}
 
-    @Override
-    public InteractionResult use(Level level, Player player, InteractionHand hand) {
-        player.startUsingItem(hand);
-        return InteractionResult.CONSUME;
-    }
+	@Override
+	public InteractionResult use(Level level, Player player, InteractionHand hand) {
+		player.startUsingItem(hand);
+		return InteractionResult.CONSUME;
+	}
 
-    @Override
-    public ItemUseAnimation getUseAnimation(ItemStack stack) {
-        return ItemUseAnimation.BOW;
-    }
+	@Override
+	public ItemUseAnimation getUseAnimation(ItemStack stack) {
+		return ItemUseAnimation.BOW;
+	}
 
-    @Override
-    public int getUseDuration(ItemStack stack, LivingEntity entity) {
-        return MAX_USE_DURATION;
-    }
+	@Override
+	public int getUseDuration(ItemStack stack, LivingEntity entity) {
+		return MAX_USE_DURATION;
+	}
 
-    @Override
-    public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
-        List<Entity> stuff = EntityUtils.getEntitiesInRange(pLivingEntity.level(), pLivingEntity.position(), 15.0F, pLivingEntity, EntityType.ITEM);
-        stuff.forEach(e -> {
-            if (e.isAlive()) {
-                double d6 = e.getX() - pLivingEntity.getX();
-                double d7 = e.getY() - pLivingEntity.getY() + pLivingEntity.getEyeHeight() / 2.0f;
-                double d8 = e.getZ() - pLivingEntity.getZ();
-                double d9 = Math.sqrt(d6 * d6 + d7 * d7 + d8 * d8);
-                d6 /= d9;
-                d7 /= d9;
-                d8 /= d9;
-                double d10 = 0.4;
-                double vX = Mth.clamp(e.getDeltaMovement().x() - d6 * d10, -0.35D, 0.35D);
-                double vY = Mth.clamp(e.getDeltaMovement().y() - d7 * d10 - 0.1, -0.35D, 0.35D);
-                double vZ = Mth.clamp(e.getDeltaMovement().z() - d8 * d10, -0.35D, 0.35D);
-                e.setDeltaMovement(vX, vY, vZ);
-                if(pLevel.isClientSide()) {
-                    RandomSource rand = pLevel.getRandom();
-                    pLevel.addParticle(ParticleTypes.WITCH,
-                            e.getX() + (rand.nextFloat() - rand.nextFloat()) * 0.3f,
-                            e.getY() + rand.nextFloat() * 0.5f,
-                            e.getZ() + (rand.nextFloat() - rand.nextFloat()) * 0.3f,
-                            (rand.nextFloat() - 0.5f) * 0.1f,
-                            rand.nextFloat() * 0.05f,
-                            (rand.nextFloat() - 0.5f) * 0.1f);
-                }
-            }
-        });
-    }
+	@Override
+	public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+		List<Entity> stuff = EntityUtils.getEntitiesInRange(pLivingEntity.level(), pLivingEntity.position(), 15.0F, pLivingEntity, EntityType.ITEM);
+		stuff.forEach(e -> {
+			if (e.isAlive()) {
+				double d6 = e.getX() - pLivingEntity.getX();
+				double d7 = e.getY() - pLivingEntity.getY() + pLivingEntity.getEyeHeight() / 2.0f;
+				double d8 = e.getZ() - pLivingEntity.getZ();
+				double d9 = Math.sqrt(d6 * d6 + d7 * d7 + d8 * d8);
+				d6 /= d9;
+				d7 /= d9;
+				d8 /= d9;
+				double d10 = 0.4;
+				double vX = Mth.clamp(e.getDeltaMovement().x() - d6 * d10, -0.35D, 0.35D);
+				double vY = Mth.clamp(e.getDeltaMovement().y() - d7 * d10 - 0.1, -0.35D, 0.35D);
+				double vZ = Mth.clamp(e.getDeltaMovement().z() - d8 * d10, -0.35D, 0.35D);
+				e.setDeltaMovement(vX, vY, vZ);
+				if (pLevel.isClientSide()) {
+					RandomSource rand = pLevel.getRandom();
+					pLevel.addParticle(ParticleTypes.WITCH,
+							e.getX() + (rand.nextFloat() - rand.nextFloat()) * 0.3f,
+							e.getY() + rand.nextFloat() * 0.5f,
+							e.getZ() + (rand.nextFloat() - rand.nextFloat()) * 0.3f,
+							(rand.nextFloat() - 0.5f) * 0.1f,
+							rand.nextFloat() * 0.05f,
+							(rand.nextFloat() - 0.5f) * 0.1f);
+				}
+			}
+		});
+	}
 }
